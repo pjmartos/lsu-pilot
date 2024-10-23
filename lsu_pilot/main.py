70761e63cd131480320119ccfcfd08be30b0f69e
Pedro Javier Martos Velasco <pmartos@theworkshop.com>
2024-10-23 23:15:36 +0200

    Retrieval Augmented Generation, Tokens, and Embeddings

--- lsu_pilot/main.py
+++ lsu_pilot/main.py
@@ -4,6 +4,17 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
 from openai import OpenAI
+import pandas as pd
+import numpy as np
+from .questions import answer_question
+
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the absolute path to the CSV file
+csv_path = os.path.join(current_dir, "processed", "embeddings.csv")
+df = pd.read_csv(csv_path, index_col=0)
+df["embeddings"] = df["embeddings"].apply(eval).apply(np.array)
 
 load_dotenv()  # take environment variables from .env.
 
@@ -34,13 +45,19 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
   await context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=completion_answer.content)
 
+async def mozilla(update: Update, context: ContextTypes.DEFAULT_TYPE):
+      answer = answer_question(df, question=update.message.text, debug=True)
+      await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+
 if __name__ == '__main__':
   application = ApplicationBuilder().token(tg_bot_token).build()
 
   start_handler = CommandHandler('start', start)
   chat_handler = CommandHandler('chat', chat)
+  mozilla_handler = CommandHandler('mozilla', mozilla)
 
   application.add_handler(start_handler)
   application.add_handler(chat_handler)
+  application.add_handler(mozilla_handler)
 
   application.run_polling()
