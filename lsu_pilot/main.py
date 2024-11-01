d464f56370bce9dc8b0225c5a6e4d417773ea5fd
Pedro Javier Martos Velasco <pmartos@theworkshop.com>
2024-11-01 18:21:19 +0100

    Image Generation with Dall-E

--- lsu_pilot/main.py
+++ lsu_pilot/main.py
@@ -1,3 +1,4 @@
+import requests
 from .functions import functions, run_function
 import json
 import os
@@ -133,15 +134,31 @@ async def mozilla(update: Update, context: ContextTypes.DEFAULT_TYPE):
       answer = answer_question(df, question=update.message.text, debug=True)
       await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
 
+async def image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+      response = openai.images.generate(
+          prompt=update.message.text,
+          model="dall-e-3",
+          n=1,
+          size="1024x1024"
+      )
+      image_url = response.data[0].url
+      image_response = requests.get(image_url)
+      await context.bot.send_photo(
+          chat_id=update.effective_chat.id,
+          photo=image_response.content
+      )
+
 if __name__ == '__main__':
   application = ApplicationBuilder().token(tg_bot_token).build()
 
   start_handler = CommandHandler('start', start)
   chat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), chat)
   mozilla_handler = CommandHandler('mozilla', mozilla)
+  image_handler = CommandHandler('image', image)
 
   application.add_handler(start_handler)
   application.add_handler(chat_handler)
   application.add_handler(mozilla_handler)
+  application.add_handler(image_handler)
 
   application.run_polling()
